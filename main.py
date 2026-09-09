2a3cdb6cda806f54b6f8df547cfe4e7d363f2759
Влад <seredavlad007@gmail.com>
2026-09-09 16:28:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("Введите ваше имя: ")
+print(f"Привет, {name}! Добро пожаловать в мир Python!")
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+print(f"Сумма: {num1 + num2}")
